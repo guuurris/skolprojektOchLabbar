35a5e37af3870935323156b88770ae26c9889524
guuurris <wiiala.gustav@gmail.com>
2016-05-03 14:35:26 +0200

    Update index.php

--- IB9090C Webutveckling Serversidan/del2/3.1/index.php
+++ IB9090C Webutveckling Serversidan/del2/3.1/index.php
@@ -1,34 +1,35 @@
 <?php
-	   //Inkludera smarty
-	   include('libs/Smarty.class.php');
+//Inkludera smarty
+include('libs/Smarty.class.php');
 
-	   // skapar smarty objekt
-	   $smarty = new Smarty;
+// skapar smarty objekt
+$smarty = new Smarty;
+
+$filename = 	"antalbesok.txt";
+$guests = 0;	
+$handler = fopen($filename,"r");
+if(flock($handler, LOCK_EX))
+{
+	if(file_exists($filename))
+	{	
+		$guests = (int) fread($handler,filesize($filename));
+	}
+	fclose($handler);
+	
+	$guests ++;
+	$handler = fopen($filename,"w+");
+	fwrite($handler,$guests);
+	flock($handler, LOCK_UN);
+	fclose($handler);
+   
+}	
 	   
-	   $filename = 	"antalbesok.txt";
-	   $guests = 0;	
-	   $handler = fopen($filename,"r");
-	   if(flock($handler, LOCK_EX))
-	   {
-			if(file_exists($filename))
-			{	
-				$guests = (int) fread($handler,filesize($filename));
-			}
-			fclose($handler);
-	   
-			$guests ++;
-			$handler = fopen($filename,"w+");
-			fwrite($handler,$guests);
-			flock($handler, LOCK_UN);
-			fclose($handler);
-		}	
-	   
 
 
-		// Länkar variabeln visitor med guest genom smarty 
-		$smarty->assign('visitor', $guests);
+// LÃ¤nkar variabeln visitor med guest genom smarty 
+$smarty->assign('visitor', $guests);
 
-		// Visar templaten med data
-		$smarty->display('../index.tpl.htm');
+// Visar templaten med data
+$smarty->display('../index.tpl.htm');
 	   
 ?>
